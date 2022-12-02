6d0222464ed2ad91edb6f8670a2e4d55317c0993
Chrissem98 <62968995+Chrissem98@users.noreply.github.com>
2022-12-02 01:32:42 +0000

    adding dist

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vue-todo-list-tailwind-pwa/sw.js', { scope: '/vue-todo-list-tailwind-pwa/' })})}
