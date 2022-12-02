71b4e86ce4735c44830222f8af541bc3eeb92f2f
Chrissem98 <62968995+Chrissem98@users.noreply.github.com>
2022-12-02 02:35:49 +0000

    configuration of manifest

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue-todo-list-tailwind-pwa',
   plugins: [
     vue(),
     VitePWA({ 
@@ -16,30 +17,46 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}']
       },
+      includeAssets: ['favicon.ico', 'android-chrome-192x192.png', 'android-chrome-512x512.png', 'apple-touch-icon.png', 'favicon-16x16.png', 'favicon-32x32.png'],
       manifest: {
         name: 'My Todo App',
-        short_name: 'TodoApp',
+        short_name: 'My Todo App',
         description: 'With this can help you',
-        // start_url: "/vue-example-pwa/?source=pwa",
-        // scope: "/vue-example-pwa/",
+        start_url: "/vue-todo-list-tailwind-pwa/?source=pwa",
+        scope: "/vue-todo-list-tailwind-pwa/",
         display: "standalone",
         orientation: "portrait",
-        // theme_color: '#121212',
+        theme_color: '#1e3a8a',
+        background_color: '#1e3a8a',
         categories: [
           "Application",
+          "education",
+          "utilities"
         ],
         icons: [
           {
-            src: 'vue-192x192.png',
+            src: 'android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'vue-512x512.png',
+            src: 'android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
         ],
+        screenshots: [
+          {
+            "src": "screen-1.png",
+            "sizes": "400x865",
+            "type": "image/png"
+          },
+          {
+            "src": "screen-2.png",
+            "sizes": "400x865",
+            "type": "image/png"
+          },
+        ]
       }
      }
     )
